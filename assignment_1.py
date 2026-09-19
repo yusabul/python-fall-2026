d670a0b01c3c95a21bca1e4effb91496c418dce5
yusabul <yusuf.abulghaith@gmail.com>
2026-09-19 12:38:40 -0400

    Added weather code

--- assignment_1.py
+++ assignment_1.py
@@ -1,1 +1,22 @@
 print("Salam from our first class!")
+
+# sample_weather.py
+def get_todays_weather():
+    weather = {
+        "location": "Sterling, VA",
+        "current_temp": "68°F",
+        "high": "78°F",
+        "low": "68°F",
+        "conditions": "Mostly cloudy",
+        "wind": "NNE 5 mph",
+        "air_quality": "Fair"
+    }
+    return weather
+if __name__ == "__main__":
+    w = get_todays_weather()
+    print(f"Today's Weather for {w['location']}")
+    print(f"Current Temperature: {w['current_temp']}")
+    print(f"High: {w['high']} | Low: {w['low']}")
+    print(f"Conditions: {w['conditions']}")
+    print(f"Wind: {w['wind']}")
+    print(f"Air Quality: {w['air_quality']}")
