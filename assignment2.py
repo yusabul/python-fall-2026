4e8fb0bd7d5ca80852ab0ac4be66368356636f56
yusabul <yusuf.abulghaith@gmail.com>
2026-09-26 11:23:26 -0400

    Assignment 2

---
+++ assignment2.py
@@ -0,0 +1,10 @@
+if 5 > 6:
+    print("5 is greater than 6")
+else:
+    print("5 is less than 6")
+
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+
+print("Hello World!"); print("Have a good day."); print("Learning Python is fun!")
